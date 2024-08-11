681571e9ede2592c9c0d8ec3ce73666d4a4f5af6
raquelrodriguez84 <rakela290@gmail.com>
2024-08-11 19:26:02 +0200

    enlace contacto.js

--- script/contacto.js
+++ script/contacto.js
@@ -1,5 +1,5 @@
 //importo las funciones neceserias barra de navegacion, footer y el mapa dinamico
-import { creaNav, creaFooter, mapa} from "export/script.js";
+import { creaNav, creaFooter, mapa} from "./export/script.js";
 document.addEventListener('DOMContentLoaded', function(){
     creaNav();
     creaFooter();
