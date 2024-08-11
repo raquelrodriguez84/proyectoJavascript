7c94a78355c05b3ad6bd51456d6a86d887aef5cf
raquelrodriguez84 <rakela290@gmail.com>
2024-08-11 19:24:50 +0200

    enalace galeria.js

--- script/galeria.js
+++ script/galeria.js
@@ -1,5 +1,5 @@
 //importo las funciones neceserias barra de navegacion, footer y el lightbox
-import { creaNav, creaFooter } from "export/script.js";
+import { creaNav, creaFooter } from "./export/script.js";
 document.addEventListener('DOMContentLoaded', function(){
     creaNav();
     creaFooter();
